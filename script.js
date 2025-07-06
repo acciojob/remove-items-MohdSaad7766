392285f4d3ac43afe294759d4220860da859f12c
Ansari-Mohd-Saad <157679873+MohdSaad7766@users.noreply.github.com>
2025-07-06 14:11:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+function removeColor() {
+    // Get the select element
+    const colorSelect = document.getElementById("colorSelect");
+    
+    // Get the selected index
+    const selectedIndex = colorSelect.selectedIndex;
+    
+    // Remove the selected option if one is selected
+    if (selectedIndex !== -1) {
+        colorSelect.remove(selectedIndex);
+    }
+}
